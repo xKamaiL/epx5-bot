46cc75bbc9fce96aad2c1ea1038371ac798600a9
xKamaiL <xkamail@icloud.com>
2022-05-11 18:19:47 +0700

    feat: dummy css style and homepage

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -1,9 +1,13 @@
 <script>
 	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
 <svelte:head />
+<svelte:body class="bg-darken-200" />
+
 <div class="">
+	<Navbar />
 	<div class="bg-page md:mt-[105px] mt-[15px] min-h-screen relative">
 		<div class="mx-auto container relative">
 			<div>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,7 +1,17 @@
-<script>
-	import Button from '../components/common/Button.svelte';
-</script>
-
-<h1 class="bg-pink-500">Welcome to SvelteKit</h1>
-
-<Button color="blue" to="/a">test</Button>
+<section class="py-10 md:pb-32 bg-white">
+	<div class="container px-4 mx-auto">
+		<div class="md:max-w-4xl mb-12 mx-auto text-center">
+			<span
+				class="inline-block py-px px-2 mb-4 text-xs leading-5 text-green-500 bg-green-100 font-medium uppercase rounded-full shadow-sm"
+				>Features</span
+			>
+			<h1 class="mb-4 text-3xl md:text-4xl leading-tight font-bold tracking-tighter">
+				Gain more insight into how people use your
+			</h1>
+			<p class="text-lg md:text-xl text-coolGray-500 font-medium">
+				With our integrated CRM, project management, collaboration and invoicing capabilities, you
+				can manage every aspect of your business in one secure platform.
+			</p>
+		</div>
+	</div>
+</section>
