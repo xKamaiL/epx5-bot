eb555c8e4faddd78f802ac7f641247290d328863
xKamaiL <xkamail@icloud.com>
2022-05-12 00:51:08 +0700

    optimize some ui

--- web/src/components/common/Button.svelte
+++ web/src/components/common/Button.svelte
@@ -1,15 +1,16 @@
 <script>
 	export let color = 'primary';
 	export let to = '';
+	export let size = 'md';
 	export const className = `btn btn-${color}`;
 </script>
 
 {#if to !== ''}
-	<a href={to} class="{className}">
+	<a href={to} class={className}>
 		<slot />
 	</a>
 {:else}
-	<button class="{className}">
+	<button class={className}>
 		<slot />
 	</button>
 {/if}
--- web/src/routes/f/[...path].svelte
+++ web/src/routes/f/[...path].svelte
@@ -17,6 +17,8 @@
 	import { FileManagerService } from '../../services';
 	import { fileList } from '../../store.ts';
 	import { toast } from '@zerodevx/svelte-toast';
+	import Button from '../../components/common/Button.svelte';
+	import dayjs from 'dayjs';
 
 	export let path;
 	export let data = [];
@@ -70,29 +72,33 @@
 	}
 </script>
 
-<h1 class="text-4xl divide-gray-500 ">
-	<a href="/f/" class="hover:text-primary-500 font-bold">Folder</a>
-	<span class="">
-		{#each path.split(`/`) as p, i}
-			<span class="text-gray-500">/</span>
-			<a
-				href="/f{cleanPath(path.split(`/`).splice(0, i).join(`/`))}/{p}"
-				class="hover:text-primary-500 font-bold"
-			>
-				{p}
-			</a>
-		{/each}
-	</span>
-</h1>
+<div class="grid grid-cols-12">
+	<div class="col-span-6">
+		<h1 class="text-4xl divide-gray-500 ">
+			<a href="/f/" class="hover:text-primary-500 font-bold">Folder</a>
+			<span class="">
+				{#each path.split(`/`) as p, i}
+					<span class="text-gray-500">/</span>
+					<a
+						href="/f{cleanPath(path.split(`/`).splice(0, i).join(`/`))}/{p}"
+						class="hover:text-primary-500 font-bold"
+					>
+						{p}
+					</a>
+				{/each}
+			</span>
+		</h1>
+	</div>
+	<div class="col-span-6 flex md:justify-end space-y-4">
+		<Button>Upload</Button>
+		<input placeholder="Search..." />
+	</div>
+</div>
 <div class=" h-2 w-full" />
 
 <div class="px-1 py-5 shadow rounded">
 	{#each data as item}
-		<a
-			on:click={routeToPathOrFile(`/f/${upperPathCheck(item.originalName)}`, item)}
-			href={`/f/${upperPathCheck(item.originalName)}`}
-			class=""
-		>
+		<a>
 			<div
 				class="p-2 divide-amber-300  cursor-pointer  group {item.originalName === path + `/`
 					? `mb-2 bg-gray-200 rounded hover:bg-gray-300`
@@ -101,24 +107,33 @@
 				<div class="grid grid-cols-12">
 					<div class="col-span-9">
 						<div class="flex-row flex">
-							<div class="icon mr-2">
+							<div class="icon ">
 								{#if item.type === 0}
 									<i class="fas fa-file" />
 								{:else}
 									<i class="fas fa-folder" />
 								{/if}
 							</div>
-							<span class="text-primary-500 group-hover:text-primary-700">
+							<a
+								on:click={routeToPathOrFile(`/f/${upperPathCheck(item.originalName)}`, item)}
+								href={`/f/${upperPathCheck(item.originalName)}`}
+								class="text-primary-500 group-hover:text-primary-700 w-1/2"
+							>
 								{#if item.name === '/' && item.type === 1}
 									{item.originalName}
 								{:else}
 									{item.name}
 								{/if}
-							</span>
+							</a>
+							<i class="fa fa-user" />
+							{item.owner.split('user-')[1].toLowerCase()}
 						</div>
 					</div>
-					<div class="col-span-3">
-						{item.createdAt}
+					<div class="col-span-3 text-right">
+						<Button>
+							<i class="fa fa-download" />
+						</Button>
+						{dayjs(item.createdAt).format(`DD/MM/YYYY HH:mm`)}
 					</div>
 				</div>
 			</div>
