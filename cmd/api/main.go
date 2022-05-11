21a4c9705091997f07f0c84f4034fb0dc28793a6
xKamaiL <xkamail@icloud.com>
2022-05-11 19:30:44 +0700

    feat: upload, create folder , list dir

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net"
 	"net/http"
@@ -34,8 +35,10 @@ func readEnv(key, defaultValue string) string {
 func run(ctx context.Context) error {
 	projectID := readEnv("FIREBASE_PROJECT_ID", "")
 	port := readEnv("PORT", "8080")
+	defaultBucket := readEnv("FIREBASE_BUCKET_NAME", "epx5-2bfff.appspot.com")
 
-	app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: projectID})
+	app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: projectID, StorageBucket: defaultBucket})
+
 	if err != nil {
 		return err
 	}
@@ -93,21 +96,61 @@ func run(ctx context.Context) error {
 
 func handlers() http.Handler {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
-	})
-	r.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		_, fh, err := r.FormFile("file")
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if _, err := cloud.Upload(ctx, "test test", fh); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println("cloud upload error: ", err)
-			return
-		}
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello world"))
+		})
+		r.Route("/file", func(r chi.Router) {
+			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				var p cloud.ListParam
+				p.Prefix = r.URL.Query().Get("prefix")
+				result, err := cloud.List(r.Context(), p)
+				if err != nil {
+					JSONErr(w, err)
+					return
+				}
+				JSON(w, result)
+			})
+			r.Post("/folder", func(w http.ResponseWriter, r *http.Request) {
+				var p struct {
+					Name string `json:"name"`
+				}
+				if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+					JSONErr(w, err)
+					return
+				}
+				if err := cloud.CreateFolder(r.Context(), p.Name); err != nil {
+					JSONErr(w, err)
+					return
+				}
+				JSON(w, nil)
+			})
+			r.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
+				ctx := r.Context()
+				_, fh, err := r.FormFile("file")
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				if _, err := cloud.Upload(ctx, "test test", fh); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					log.Println("cloud upload error: ", err)
+					return
+				}
+			})
+		})
+
 	})
 	return r
+}
+
+func JSONErr(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	JSON(w, map[string]any{
+		"message": err.Error(),
+	})
+}
+
+func JSON(w http.ResponseWriter, i any) {
+	json.NewEncoder(w).Encode(i)
 }
--- internal/cloud/cloud.go
+++ internal/cloud/cloud.go
@@ -1,6 +1,7 @@
 package cloud
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -8,8 +9,10 @@ import (
 	"log"
 	"mime/multipart"
 	"path"
+	"strings"
 	"time"
 
+	"cloud.google.com/go/storage"
 	"google.golang.org/api/iterator"
 )
 
@@ -18,13 +21,53 @@ var (
 	ErrUploadFailed    = errors.New("upload failed")
 )
 
-func List(ctx context.Context) (any, error) {
+type FileType int
+
+const (
+	FileTypeFile FileType = iota
+	FileTypeFolder
+)
+
+type File struct {
+	Name         string    `json:"name"`
+	Type         FileType  `json:"type"`
+	CreatedAt    time.Time `json:"createdAt"`
+	Prefix       string    `json:"prefix"`
+	Size         int64     `json:"size"`
+	ContentType  string    `json:"contentType"`
+	Owner        string    `json:"owner"`
+	OriginalName string    `json:"originalName"`
+}
+type ListParam struct {
+	Prefix string `json:"prefix"`
+	Limit  int    `json:"limit"`
+}
+
+func (p *ListParam) Valid() error {
+	p.Prefix = strings.ToLower(strings.TrimSpace(p.Prefix))
+	if p.Prefix == "" {
+		//p.Prefix = "/"
+	}
+	return nil
+}
+
+func List(ctx context.Context, q ListParam) ([]*File, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	if err := q.Valid(); err != nil {
+		return nil, err
+	}
+
 	bucket, err := client(ctx).DefaultBucket()
 	if err != nil {
 		return nil, err
 	}
 
-	it := bucket.Objects(ctx, nil)
+	it := bucket.Objects(ctx, &storage.Query{
+		Prefix: q.Prefix,
+	})
+	dir := make([]*File, 0)
 	for {
 		attrs, err := it.Next()
 		if err == iterator.Done {
@@ -33,9 +76,61 @@ func List(ctx context.Context) (any, error) {
 		if err != nil {
 			return nil, fmt.Errorf("Bucket(%q).Objects: %v", bucket, err)
 		}
-		log.Println(attrs.Name)
+		if attrs.Name == q.Prefix {
+			continue
+		}
+
+		fileType := FileTypeFile
+
+		name := strings.Replace(attrs.Name, q.Prefix, "", 1)
+		if strings.Contains(name, "/") && !strings.Contains(name, ".") {
+			fileType = FileTypeFolder
+		}
+		// skip file in sub folder
+		if strings.Count(name, "/") >= 2 && strings.HasSuffix(name, "/") {
+			continue
+		}
+
+		dir = append(dir, &File{
+			OriginalName: attrs.Name,
+			Name:         name,
+			Type:         fileType,
+			CreatedAt:    attrs.Created,
+			Prefix:       attrs.Prefix,
+			Size:         attrs.Size,
+			ContentType:  attrs.ContentType,
+			Owner:        attrs.Owner,
+		})
 	}
-	return nil, nil
+
+	return dir, nil
+}
+
+func CreateFolder(ctx context.Context, name string) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	name = strings.TrimSuffix(strings.TrimPrefix(name, "/"), "/") + "/"
+
+	// TODO: validate name
+	bucket, err := client(ctx).DefaultBucket()
+	if err != nil {
+		return err
+	}
+	log.Println("create", name)
+	wc := bucket.Object(name).If(storage.Conditions{DoesNotExist: true}).NewWriter(ctx)
+	wc.ContentType = "application/x-www-form-urlencoded;charset=UTF-8"
+	src := bytes.NewBufferString("")
+
+	if _, err = io.Copy(wc, src); err != nil {
+		log.Printf("storage io.Copy error; %v", err)
+		return ErrUploadFailed
+	}
+	if err := wc.Close(); err != nil {
+		log.Printf("storage wc.Copy error; %v", err)
+		return ErrUploadFailed
+	}
+	return nil
 }
 
 func Upload(ctx context.Context, pathname string, fh *multipart.FileHeader) (any, error) {
@@ -60,7 +155,7 @@ func Upload(ctx context.Context, pathname string, fh *multipart.FileHeader) (any
 	}
 
 	// Upload an object with storage.Writer.
-	wc := bucket.Object(fileName).NewWriter(ctx)
+	wc := bucket.Object(fileName).If(storage.Conditions{DoesNotExist: true}).NewWriter(ctx)
 	if _, err := io.Copy(wc, file); err != nil {
 		log.Printf("storage io.Copy error; %v", err)
 		return nil, ErrUploadFailed
